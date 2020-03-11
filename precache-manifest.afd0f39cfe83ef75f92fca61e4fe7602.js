b238dc6051a06fe1e511324d8eb8a5fbd340c1d0
juheesvt <38813640+juheesvt@users.noreply.github.com>
2020-03-11 13:04:37 +0900

    Updates

---
+++ precache-manifest.afd0f39cfe83ef75f92fca61e4fe7602.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6a9bd3e4f32aac33c6d7c49498e972",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "1f5549ae0c36c81d0475",
+    "url": "/movie_app_2019/static/css/main.a1fe5f25.chunk.css"
+  },
+  {
+    "revision": "86325af0249feaa2a644",
+    "url": "/movie_app_2019/static/js/2.9ad0a64e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_2019/static/js/2.9ad0a64e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f5549ae0c36c81d0475",
+    "url": "/movie_app_2019/static/js/main.cdab07de.chunk.js"
+  },
+  {
+    "revision": "389edc25852210aa1501",
+    "url": "/movie_app_2019/static/js/runtime-main.3c6837db.js"
+  }
+]);
